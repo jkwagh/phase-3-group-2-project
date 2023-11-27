ba1da74697dda19088c2a2acc479f5f9e638d5d6
Jeremy <kwagh.jeremiah@gmail.com>
2023-11-27 13:29:25 -0500

    added helper functions and updated menu

--- lib/helpers.py
+++ lib/helpers.py
@@ -7,3 +7,19 @@ def helper_1():
 def exit_program():
     print("Goodbye!")
     exit()
+    
+def create_workout():
+    pass
+
+def workout_log():
+    pass
+
+def show_all_workouts():
+    pass
+
+def show_all_users():
+    pass
+
+def delete_workout():
+    pass
+
