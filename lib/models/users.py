deac88e00a28b6ed3522d430b5abea4f00eeae7c
Matthew Tran <matt.h.tran@gmail.com>
2023-11-27 14:24:23 -0500

    users.py update with age

--- lib/models/users.py
+++ lib/models/users.py
@@ -1,10 +1,13 @@
 class Users:
-    def __init__(self, name):
+    def __init__(self, name, age, fitness_goals):
         self.name = name
+        self.age = age
+        self.fitness_goals
         
     @property
     def name(self):
         return self._name
+
     @name.setter
     def name(self, name):
         if isinstance(name, str) and len(name):
