592b7c1db246f9d887bcd1ec66b51cff44739e21
Matthew Tran <matt.h.tran@gmail.com>
2023-11-28 12:04:58 -0500

    updated exercise

--- lib/models/users.py
+++ lib/models/users.py
@@ -1,19 +1,50 @@
-class Users:
-    def __init__(self, name, age, fitness_goals):
-        self.name = name
-        self.age = age
-        self.fitness_goals = fitness_goals
-        
+class Exercise:
+    def __init__(self, name, exercise_type, difficulty, sets, reps):
+        self._name = name
+        self._exercise_type = exercise_type
+        self._difficulty = difficulty
+        self._sets = sets
+        self._reps = reps
+
     @property
     def name(self):
         return self._name
 
     @name.setter
-    def name(self, name):
-        if isinstance(name, str) and len(name):
-            self._name = name
+    def name(self, value):
+        if isinstance(value, str) and len(value):
+            self._name = value
         else:
-            print("name must be a non-empty string")
+            print("Name must be a non-empty string")
 
-user1 = Users("Steve", 25, "Build muscle")
-print(user1.name)
+    @property
+    def exercise_type(self):
+        return self._exercise_type
+
+    @exercise_type.setter
+    def exercise_type(self, value):
+        self._exercise_type = value
+
+    @property
+    def difficulty(self):
+        return self._difficulty
+
+    @difficulty.setter
+    def difficulty(self, value):
+        self._difficulty = value
+
+    @property
+    def sets(self):
+        return self._sets
+
+    @sets.setter
+    def sets(self, value):
+        self._sets = value
+
+    @property
+    def reps(self):
+        return self._reps
+
+    @reps.setter
+    def reps(self, value):
+        self._reps = value
