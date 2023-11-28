c3eb00693b6aaa1c25da6992a3b8bc2bf15f8817
Jeremy <kwagh.jeremiah@gmail.com>
2023-11-27 21:01:30 -0500

    update Users

--- lib/models/users.py
+++ lib/models/users.py
@@ -7,13 +7,35 @@ class Users:
     @property
     def name(self):
         return self._name
-
     @name.setter
     def name(self, name):
         if isinstance(name, str) and len(name):
             self._name = name
         else:
-            print("name must be a non-empty string")
+            print("Name must be a non-empty string")
+            
+    @property
+    def age(self):
+        return self._age
+    @age.setter
+    def age(self, age):
+        if isinstance(age, int) and 1 < age < 99:
+            self._age = age
+        else:
+            print("Age must be a number between 1 and 99")
+            
+    @property
+    def fitness_goals(self):
+        return self._fitness_goals
+    @fitness_goals.setter
+    def fitness_goals(self, fitness_goals):
+        if isinstance(fitness_goals, str) and len(fitness_goals) < 140:
+            self._fitness_goals = fitness_goals
+        else:
+            print("Fitness goals must be a string less than 140 characters")
 
-user1 = Users("Steve", 25, "Build muscle")
+    
+user1 = Users("Steve", 25, "To gain muscle")
 print(user1.name)
+print(user1.age)
+print(user1.fitness_goals)
