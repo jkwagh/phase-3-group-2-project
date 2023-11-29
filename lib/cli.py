5afaa01c1cc9166300e6e3aa15ba82c7b67c65ff
Matthew Tran <matt.h.tran@gmail.com>
2023-11-29 11:38:45 -0500

    updates to cli

--- lib/cli.py
+++ lib/cli.py
@@ -4,10 +4,8 @@ from sqlalchemy.orm import sessionmaker
 from models import Base, User, Workout, Exercise
 from database import setup_database
 
-# Call setup_database to get the engine
 DBSession, engine = setup_database()
 
-# Create a session
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
@@ -17,125 +15,87 @@ def cli():
     """Fitness Tracker CLI"""
 
 @cli.command()
-@click.option('--name', prompt='Enter your name', help='Your name')
-@click.option('--age', prompt='Enter your age', type=int, help='Your age')
-@click.option('--fitness_goals', prompt='Enter your fitness goals', help='Your fitness goals')
+@click.option('--name', prompt='Enter the user name', help='User name')
+@click.option('--age', prompt='Enter the age', type=int, help='User age')
+@click.option('--fitness_goals', prompt='Enter fitness goals', help='User fitness goals')
 def create_user(name, age, fitness_goals):
     """Create a new user"""
-    user = User(name=name, age=age, fitness_goals=fitness_goals)
-    session.add(user)
-    session.commit()
+    user = User.create(session, name=name, age=age, fitness_goals=fitness_goals)
     click.echo(f"User {name} created successfully with ID: {user.id}")
 
 @cli.command()
-@click.option('--user_id', prompt='Enter your user ID', type=int, help='Your user ID')
-@click.option('--date', prompt='Enter the date (YYYY-MM-DD)', help='Workout date')
-@click.option('--duration', prompt='Enter the duration in minutes', type=int, help='Workout duration')
-def log_workout(user_id, date, duration):
-    """Log a workout"""
-    user = session.query(User).filter_by(id=user_id).first()
-    if not user:
-        click.echo(f"User with ID {user_id} not found.")
-        return
-
-    workout = Workout(date=datetime.strptime(date, '%Y-%m-%d'), duration=duration, user=user)
-    session.add(workout)
-    session.commit()
-
-    click.echo(f"Workout logged for {user.name} on {date} for {duration} minutes.")
+@click.option('--user_id', prompt='Enter the user ID to delete', type=int, help='User ID to delete')
+def delete_user(user_id):
+    """Delete a user"""
+    User.delete(session, user_id)
+    click.echo(f"User with ID {user_id} deleted successfully!")
 
 @cli.command()
-def view_workouts():
-    """View all workouts"""
-    workouts = session.query(Workout).all()
-    for workout in workouts:
-        click.echo(f"Workout ID: {workout.id}, Date: {workout.date}, Duration: {workout.duration} minutes")
+@click.option('--user_id', prompt='Enter the user ID', type=int, help='User ID')
+@click.option('--date', prompt='Enter the date (YYYY-MM-DD)', help='Workout date')
+@click.option('--duration', prompt='Enter the duration in minutes', type=int, help='Workout duration')
+def add_workout(user_id, date, duration):
+    """Add a workout"""
+    workout = Workout.create(session, date=datetime.strptime(date, '%Y-%m-%d'), duration=duration, user_id=user_id)
+    click.echo(f"Workout logged for user ID {user_id} on {date} for {duration} minutes with ID: {workout.id}")
 
 @cli.command()
 @click.option('--workout_id', prompt='Enter the workout ID to delete', type=int, help='Workout ID to delete')
 def delete_workout(workout_id):
     """Delete a workout"""
-    workout = session.query(Workout).filter_by(id=workout_id).first()
-    if not workout:
-        click.echo(f"Workout with ID {workout_id} not found.")
-        return
-
-    session.delete(workout)
-    session.commit()
+    Workout.delete(session, workout_id)
     click.echo(f"Workout with ID {workout_id} deleted successfully!")
 
 @cli.command()
-def add_exercise():
-    # Command to add an exercise
-    name = click.prompt("Enter exercise name", type=str)
-    type = click.prompt("Enter exercise type", type=str)
-    difficulty = click.prompt("Enter exercise difficulty", type=str)
-    sets = click.prompt("Enter number of sets", type=int)
-    reps = click.prompt("Enter number of reps", type=int)
-
-    exercise = Exercise(name=name, type=type, difficulty=difficulty, sets=sets, reps=reps)
-    session.add(exercise)
-    session.commit()
-
-    click.echo("Exercise added successfully!")
+def display_users():
+    """Display all users"""
+    users = User.get_all(session)
+    for user in users:
+        click.echo(f"User ID: {user.id}, Name: {user.name}, Age: {user.age}, Fitness Goals: {user.fitness_goals}")
 
 @cli.command()
-def delete_exercise():
-    # Command to delete an exercise
-    exercise_id = click.prompt("Enter exercise ID to delete", type=int)
-
-    exercise = session.query(Exercise).get(exercise_id)
-    if exercise:
-        session.delete(exercise)
-        session.commit()
-        click.echo("Exercise deleted successfully!")
-    else:
-        click.echo("Exercise not found.")
-
-@cli.command()
-def add_exercise_to_workout():
-    # Command to add an exercise to a workout
-    workout_id = click.prompt("Enter workout ID", type=int)
-    exercise_id = click.prompt("Enter exercise ID to add to the workout", type=int)
-
-    workout = session.query(Workout).get(workout_id)
-    exercise = session.query(Exercise).get(exercise_id)
-
-    if workout and exercise:
-        workout.exercises.append(exercise)
-        session.commit()
-        click.echo("Exercise added to the workout successfully!")
-    else:
-        click.echo("Workout or exercise not found.")
-
-@cli.command()
-def delete_exercise_from_workout():
-    # Command to delete an exercise from a workout
-    workout_id = click.prompt("Enter workout ID", type=int)
-    exercise_id = click.prompt("Enter exercise ID to delete from the workout", type=int)
-
-    workout = session.query(Workout).get(workout_id)
-    exercise = session.query(Exercise).get(exercise_id)
-
-    if workout and exercise:
-        workout.exercises.remove(exercise)
-        session.commit()
-        click.echo("Exercise deleted from the workout successfully!")
-    else:
-        click.echo("Workout or exercise not found.")
-
-@cli.command()
-@click.option('--user_id', prompt='Enter your user ID', type=int, help='Your user ID')
-def analyze_workout_history(user_id):
-    """Analyze workout history for a user"""
-    user = session.query(User).filter_by(id=user_id).first()
-    if not user:
-        click.echo(f"User with ID {user_id} not found.")
-        return
-
-    click.echo(f"Workout history for {user.name}:")
-    for workout in user.workouts:
+def display_workouts():
+    """Display all workouts"""
+    workouts = Workout.get_all(session)
+    for workout in workouts:
         click.echo(f"Workout ID: {workout.id}, Date: {workout.date}, Duration: {workout.duration} minutes")
+
+@cli.command()
+def display_exercises():
+    """Display all exercises"""
+    exercises = Exercise.get_all(session)
+    for exercise in exercises:
+        click.echo(f"Exercise ID: {exercise.id}, Name: {exercise.name}, Type: {exercise.type}, Difficulty: {exercise.difficulty}")
+
+@cli.command()
+@click.option('--user_id', prompt='Enter the user ID to find', type=int, help='User ID to find')
+def find_user(user_id):
+    """Find user by ID"""
+    user = User.find_by_id(session, user_id)
+    if user:
+        click.echo(f"User ID: {user.id}, Name: {user.name}, Age: {user.age}, Fitness Goals: {user.fitness_goals}")
+    else:
+        click.echo(f"User with ID {user_id} not found.")
+
+@cli.command()
+@click.option('--workout_id', prompt='Enter the workout ID to find', type=int, help='Workout ID to find')
+def find_workout(workout_id):
+    """Find workout by ID"""
+    workout = Workout.find_by_id(session, workout_id)
+    if workout:
+        click.echo(f"Workout ID: {workout.id}, Date: {workout.date}, Duration: {workout.duration} minutes")
+    else:
+        click.echo(f"Workout with ID {workout_id} not found.")
+
+@cli.command()
+@click.option('--exercise_id', prompt='Enter the exercise ID to find', type=int, help='Exercise ID to find')
+def find_exercise(exercise_id):
+    """Find exercise by ID"""
+    exercise = Exercise.find_by_id(session, exercise_id)
+    if exercise:
+        click.echo(f"Exercise ID: {exercise.id}, Name: {exercise.name}, Type: {exercise.type}, Difficulty: {exercise.difficulty}")
+    else:
+        click.echo(f"Exercise with ID {exercise_id} not found.")
 
 if __name__ == '__main__':
     cli()
--- lib/models.py
+++ lib/models.py
@@ -1,7 +1,7 @@
-from sqlalchemy import Date, ForeignKey, Enum
+from sqlalchemy import create_engine, Date, ForeignKey, Enum, inspect
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, CheckConstraint
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.types import Enum as EnumType
 
 Base = declarative_base()
@@ -21,7 +21,7 @@ class Exercise(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
-    type = Column(String, nullable=False)  # Change EnumType to String
+    type = Column(String, nullable=False)
     difficulty = Column(Integer, nullable=False)
     sets = Column(Integer, nullable=False)
     reps = Column(Integer, nullable=False)
@@ -33,6 +33,28 @@ class Exercise(Base):
 
     workouts = relationship('WorkoutExercises', back_populates='exercise')
 
+    @classmethod
+    def create(cls, session, name, exercise_type, difficulty, sets, reps):
+        exercise = cls(name=name, type=exercise_type, difficulty=difficulty, sets=sets, reps=reps)
+        session.add(exercise)
+        session.commit()
+        return exercise
+
+    @classmethod
+    def delete(cls, session, exercise_id):
+        exercise = cls.find_by_id(session, exercise_id)
+        if exercise:
+            session.delete(exercise)
+            session.commit()
+
+    @classmethod
+    def get_all(cls, session):
+        return session.query(cls).all()
+
+    @classmethod
+    def find_by_id(cls, session, exercise_id):
+        return session.query(cls).filter_by(id=exercise_id).first()
+
 class User(Base):
     __tablename__ = 'users'
 
@@ -42,6 +64,28 @@ class User(Base):
     fitness_goals = Column(String)
 
     workouts = relationship('Workout', back_populates='user')
+
+    @classmethod
+    def create(cls, session, name, age, fitness_goals):
+        user = cls(name=name, age=age, fitness_goals=fitness_goals)
+        session.add(user)
+        session.commit()
+        return user
+
+    @classmethod
+    def delete(cls, session, user_id):
+        user = cls.find_by_id(session, user_id)
+        if user:
+            session.delete(user)
+            session.commit()
+
+    @classmethod
+    def get_all(cls, session):
+        return session.query(cls).all()
+
+    @classmethod
+    def find_by_id(cls, session, user_id):
+        return session.query(cls).filter_by(id=user_id).first()
 
 class Workout(Base):
     __tablename__ = 'workouts'
@@ -54,6 +98,28 @@ class Workout(Base):
     user = relationship('User', back_populates='workouts')
     exercises = relationship('WorkoutExercises', back_populates='workout')
 
+    @classmethod
+    def create(cls, session, date, duration, user_id):
+        workout = cls(date=date, duration=duration, user_id=user_id)
+        session.add(workout)
+        session.commit()
+        return workout
+
+    @classmethod
+    def delete(cls, session, workout_id):
+        workout = cls.find_by_id(session, workout_id)
+        if workout:
+            session.delete(workout)
+            session.commit()
+
+    @classmethod
+    def get_all(cls, session):
+        return session.query(cls).all()
+
+    @classmethod
+    def find_by_id(cls, session, workout_id):
+        return session.query(cls).filter_by(id=workout_id).first()
+
 class WorkoutExercises(Base):
     __tablename__ = 'workout_exercises'
 
@@ -64,3 +130,25 @@ class WorkoutExercises(Base):
 
     exercise = relationship('Exercise', back_populates='workouts')
     workout = relationship('Workout', back_populates='exercises')
+
+    @classmethod
+    def create(cls, session, workout_id, exercise_id, sets_completed, reps_completed):
+        workout_exercise = cls(workout_id=workout_id, exercise_id=exercise_id, sets_completed=sets_completed, reps_completed=reps_completed)
+        session.add(workout_exercise)
+        session.commit()
+        return workout_exercise
+
+    @classmethod
+    def delete(cls, session, workout_id, exercise_id):
+        workout_exercise = cls.find_by_ids(session, workout_id, exercise_id)
+        if workout_exercise:
+            session.delete(workout_exercise)
+            session.commit()
+
+    @classmethod
+    def get_all(cls, session):
+        return session.query(cls).all()
+
+    @classmethod
+    def find_by_ids(cls, session, workout_id, exercise_id):
+        return session.query(cls).filter_by(workout_id=workout_id, exercise_id=exercise_id).first()
