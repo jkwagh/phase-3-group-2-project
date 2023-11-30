1faec5f4957ee630973dc794fc0cfb0419967c3c
Matthew Tran <matt.h.tran@gmail.com>
2023-11-30 10:19:16 -0500

    cli changes

--- lib/cli.py
+++ lib/cli.py
@@ -1,17 +1,38 @@
 import click
 from datetime import datetime
 from sqlalchemy.orm import sessionmaker
-from models import Base, User, Workout, Exercise
+from models import Base, User, Workout, Exercise, WorkoutExercises
 from database import setup_database
 from prettytable import PrettyTable
 import pyfiglet
 import time
+import random
 
 DBSession, engine = setup_database()
 
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
+
+quotes = [
+    "Workout more so you can eat more donuts",
+    "If you've got 99 problems, go to the gym to ignore all of them",
+    "I want to see what happens if I don't give up",
+    "You must exercise in the morning before your brain figures out what you're doing",
+    "PROTEIN,PROTEIN, PROTEIN, PROTEIN, PROTEIN, PROTEIN, PROTEIN",
+]
+
+def display_progress_bar():
+    with click.progressbar(range(10), label='Creating User') as bar:
+        for _ in bar:
+            time.sleep(0.1)
+
+def display_random_quote():
+    random_quote = random.choice(quotes)
+    colors = ['blue', 'magenta', 'cyan', 'white']
+    random_color = random.choice(colors)
+    styled_quote = click.style(random_quote, italic=True, fg=random_color)
+    click.echo(f"Gym Gods Tips (GGT): {styled_quote}")
 
 def validate_positive_integer(value, field_name):
     if value <= 0:
@@ -61,9 +82,10 @@ def create_user(name, age, fitness_goals):
     try:
         with click.progressbar(range(10), label='Creating User') as bar:
             for _ in bar:
-                time.sleep(0.1)  
-                click.clear()
-                click.echo("Creating User...")
+                time.sleep(0.1)
+        click.clear()
+        click.echo("Creating User...")
+        display_random_quote()
         user = User.create(session, name=name, age=age, fitness_goals=fitness_goals)
         click.echo(click.style(f"User {name} created successfully with ID: {user.id}", fg='green'))
     except Exception as e:
@@ -89,15 +111,35 @@ def add_workout(user_id, date, duration):
         date_obj = validate_date_format(date)
         validate_positive_integer(duration, "Duration")
         workout = Workout.create(session, date=date_obj, duration=duration, user_id=user_id)
-        click.echo(click.style(f"Workout logged for user ID {user_id} on {date} for {duration} minutes with ID: {workout.id}", fg= 'green'))
+
+        click.echo(f"Add exercises to the workout (type 'done' when finished):")
+
+        while True:
+            exercise_name = click.prompt('Enter exercise name (or type "done" to finish adding exercises)', default="done")
+
+            if exercise_name.lower() == "done":
+                break
+
+            exercise_type = click.prompt('Enter exercise type')
+            exercise_difficulty = click.prompt('Enter exercise difficulty', type=int)
+            exercise_sets = click.prompt('Enter the number of sets', type=int)
+            exercise_reps = click.prompt('Enter the number of reps', type=int)
+
+            exercise = Exercise.create(session, name=exercise_name, exercise_type=exercise_type, difficulty=exercise_difficulty, sets=exercise_sets, reps=exercise_reps)
+            WorkoutExercises.create(session, workout_id=workout.id, exercise_id=exercise.id, sets_completed=exercise_sets, reps_completed=exercise_reps)
+
+        with click.progressbar(range(10), label='Creating Workout') as bar:
+            for _ in bar:
+                time.sleep(0.1)
+        click.clear()
+        click.echo("Creating Workout...")
+
+        click.echo(click.style(f"Workout logged for user ID {user_id} on {date} for {duration} minutes with ID: {workout.id}", fg='green'))
     except ValueError as ve:
         click.echo(f"Error: {ve}")
     except Exception as e:
-        click.echo(click.style(f"Error adding workout: {e}", fg= 'red'))
-    except ValueError as ve:
-        click.echo(f"Error: {ve}")
-    except Exception as e:
-        click.echo(f"Error adding workout: {e}")
+        click.echo(click.style(f"Error adding workout: {e}", fg='red'))
+
 
 @cli.command()
 @click.option('--workout_id', prompt='Enter the workout ID to delete', type=int, help='Workout ID to delete')
